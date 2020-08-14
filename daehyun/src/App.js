8b1fc1c4c877efdbde59a796cf345a95776b3334
daelonge <dleogus0569@naver.com>
2020-08-14 16:54:57 +0900

    컴포넌트

--- daehyun/src/App.js
+++ daehyun/src/App.js
@@ -1,11 +1,8 @@
-import React, { Component } from "react";
-import "./App.css";
+import React from "react";
+import Say from "./Say";
 
-class App extends Component {
-  render() {
-    const name = "리액트";
-    return <div className="react">{name}</div>;
-  }
-}
+const App = () => {
+  return <Say />;
+};
 
 export default App;
