86dbfe64b8ffceb77e0cfdfaec4cc2eb5f1ff482
daelonge <dleogus0569@naver.com>
2020-08-17 18:02:54 +0900

    ref:dom

--- daehyun/src/App.js
+++ daehyun/src/App.js
@@ -1,8 +1,9 @@
-import React from "react";
-import EventPractice from "./EventPractice2";
+import React, { Component } from "react";
+import ValidationSample from "./ValidationSample";
 
-const App = () => {
-  return <EventPractice />;
-};
-
+class App extends Component {
+  render() {
+    return <ValidationSample />;
+  }
+}
 export default App;
