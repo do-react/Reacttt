8b0e6211f5cf873535afa2f82c1de9ced71ba482
daelong <dleogus0569@naver.com>
2020-09-04 17:37:44 +0900

    리덕스를 사용하여 리액트 애플리케이션 상태 관리하기 끝

--- react-redux-tutorial/src/containers/CounterContainer.js
+++ react-redux-tutorial/src/containers/CounterContainer.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import { connect } from 'react-redux';
+import React, { useCallback } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import Counter from '../components/Counter';
 import { increase, decrease } from '../modules/counter';
 
-const CounterContainer = ({ number, increase, decrease }) => {
+// const CounterContainer = ({ number, increase, decrease }) => {
+//   return (
+//     <Counter number={number} onIncrease={increase} onDecrease={decrease} />
+//   );
+// };
+
+// export default connect(
+//   (state) => ({
+//     number: state.counter.number,
+//   }),
+//   {
+//     increase,
+//     decrease,
+//   },
+// )(CounterContainer); //이코드는 connect를 이용해서 props를 넘겨주는거 직접 스토어의 내장함수 dispatch를 이용하는 것
+
+const CounterContainer = () => {
+  const number = useSelector((state) => state.counter.number);
+  const dispatch = useDispatch();
+  const onIncrease = useCallback(() => dispatch(increase()), [dispatch]); //useDispatch를 사용할 때는 컴포넌트 성능을 최적화 해주기위해서 useCallback과 함께 사용하는게 좋다.
+  const onDecrease = useCallback(() => dispatch(decrease()), [dispatch]);
   return (
-    <Counter number={number} onIncrease={increase} onDecrease={decrease} />
+    <Counter number={number} onIncrease={onIncrease} onDecrease={onDecrease} />
   );
 };
-
-const mapStateToProps = (state) => ({
-  number: state.counter.number,
-});
-
-const mapDispatchToProps = (dispatch) => ({
-  //임시 함수
-  increase: () => {
-    dispatch(increase());
-  },
-  decrease: () => {
-    dispatch(decrease());
-  },
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(CounterContainer);
+export default CounterContainer; // 이 코드는 useSelector로 counter.number 값을 조회함으로써 Counter에게 props를 넘겨주는거
+// 또 useDispatch Hook을 사용하여 컴포넌트 내부에서 스토어의 내장함수 dispatch를 사용할 수 있다.
--- react-redux-tutorial/src/modules/counter.js
+++ react-redux-tutorial/src/modules/counter.js
@@ -1,29 +1,28 @@
+import { createAction, handleActions } from 'redux-actions';
+
 //액션 타입 정의하기
 const INCREASE = 'counter/INCREASE'; //액션타입은 대문자로 정의, 문자열 내용은 모듈이름/액션이름과 같은 형태로 작성
 const DECREASE = 'counter/DECREASE';
 
+export const increase = createAction(INCREASE);
+export const decrease = createAction(DECREASE);
+
 //액션 생성 함수 만들기
-export const increase = () => ({ type: INCREASE }); //export는 여러개 내보낼 수 있음. export default는 단 한개만 내보낼 수 있음. 불러오는 방식도 다르다
-export const decrease = () => ({ type: DECREASE });
+// export const increase = () => ({ type: INCREASE }); //export는 여러개 내보낼 수 있음. export default는 단 한개만 내보낼 수 있음. 불러오는 방식도 다르다
+// export const decrease = () => ({ type: DECREASE });
 
 //초기 상태 및 리듀서 함수 만들기
 const initialState = {
   number: 0,
 };
 
-function counter(state = initialState, action) {
-  switch (action.type) {
-    case INCREASE:
-      return {
-        number: state.number + 1,
-      };
-    case DECREASE:
-      return {
-        number: state.number - 1,
-      };
-    default:
-      return state;
-  }
-}
+const counter = handleActions(
+  //함수의 첫번째 파라미터에 각 액션에 대한 업데이트 함수를 넣어주고 두번째 파라미터에는 초기 상태를 넣어준다.
+  {
+    [INCREASE]: (state, action) => ({ number: state.number + 1 }),
+    [DECREASE]: (state, action) => ({ number: state.number - 1 }),
+  },
+  initialState,
+);
 
 export default counter;
