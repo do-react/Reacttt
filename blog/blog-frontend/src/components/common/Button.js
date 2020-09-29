b02d967b6d27a9edb668636494649febfaa63eb2
daelong <dleogus0569@naver.com>
2020-09-29 19:14:12 +0900

    프론트엔드 프로젝트: 시작 및 회원 인증구현

---
+++ blog/blog-frontend/src/components/common/Button.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import styled, { css } from 'styled-components';
+import palette from '../../lib/styles/palette';
+
+const StyledButton = styled.button`
+  border: none;
+  border-radius: 4px;
+  font-size: 1rem;
+  font-weight: bold;
+  padding: 0.25rem 1rem;
+  color: white;
+  outline: none;
+  cursor: pointer;
+
+  background: ${palette.gray[8]};
+  &:hover {
+    background: ${palette.gray[6]};
+  }
+  ${(props) =>
+    props.fullWidth &&
+    css`
+      padding-top: 0.75rem;
+      padding-bottom: 0.75rem;
+      width: 100%;
+      font-size: 1.125rem;
+    `}
+
+  ${(props) =>
+    props.cyan &&
+    css`
+      background: ${palette.cyan[5]};
+      &:hover {
+        background: ${palette.cyan[4]};
+      }
+    `}
+`;
+
+const Button = (props) => <StyledButton {...props} />; //Buttton 리액트 컴포넌트를 만들어서 안에 StyledButton을 렌더링한건 추후 컴포넌트를 사용할 때 자동 import가 되게 하기 위해서임
+
+export default Button;
