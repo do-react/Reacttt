b6b066f1da457e65e9d6d4796799a6fbf5ab7ff8
daelong <dleogus0569@naver.com>
2020-10-05 18:07:39 +0900

    프론트엔드 프로젝트: 블로그 기능 완료

--- blog/blog-frontend/src/App.js
+++ blog/blog-frontend/src/App.js
@@ -5,10 +5,15 @@ import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import WritePage from './pages/WritePage';
 import PostPage from './pages/PostPage';
+import { Helmet } from 'react-helmet-async';
 
 const App = () => {
   return (
+    //PostLisPage처럼 path에 배열을 넣어주면 한 라우트컴포넌트에 여러개의 경로를 쉽게 설정할 수 있다.
     <>
+      <Helmet>
+        <title>대현's 블로그</title>
+      </Helmet>
       <Route component={PostListPage} path={['/@:username', '/']} exact />
       <Route component={LoginPage} path="/login" />
       <Route component={RegisterPage} path="/register" />
